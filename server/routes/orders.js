68bb690ba99f421297b99ccae1aff207f4801a36
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2023-03-30 14:10:08 -0700

    Orders by User ID implemented

---
+++ server/routes/orders.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const Order = require('../models/order');
+const User = require('../models/user');
+//const ensureAuth = require('../middleware/auth');
+//const checkUserAccess = require('../middleware/checkAccess');
+const { NotFoundError, ForbiddenError } = require('../errors');
+const router = express.Router();
+
+// Endpoint to get all orders for a user by their ID
+router.get('/:userId', async (req, res, next) => {
+	try {
+		const userId = req.params.userId;
+
+		const user = await User.findById(userId);
+
+		if (user == null) {
+			throw new NotFoundError('User not found');
+		}
+
+		if (req.user._id.toString() !== userId) {
+			throw new ForbiddenError('Access denied');
+		}
+
+		const orders = await Order.find({ user: userId });
+
+		res.status(200).json(orders);
+	} catch (error) {
+		next(error);
+	}
+});
+
+module.exports = router;
