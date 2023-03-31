b73bcf444fd54f3fa0b53c2ca09d19d42fb65da0
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2023-03-31 13:46:39 -0700

    Dummy user updated

--- server/middlewares/dummyAuth.js
+++ server/middlewares/dummyAuth.js
@@ -1,7 +1,7 @@
 function dummyAuth(req, res, next) {
 	// Set a dummy user object for development purposes
 	req.user = {
-		_id: '641d3d0d2025d607296c2cd7',
+		_id: '642652635bd781ad3b36d15c',
 		role: 'admin',
 	};
 	next();
