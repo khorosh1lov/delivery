f2a493691b2043d1371faecb39a02cee8a6489ff
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2023-04-03 01:51:09 -0700

    Chart fix days

--- server/routes/restaurants.js
+++ server/routes/restaurants.js
@@ -26,53 +26,47 @@ router.get('/count', async (req, res) => {
 
 // Endpoint for daily restaurant data
 router.get('/dailyData', async (req, res) => {
-    try {
-        const today = new Date();
-        const tenDaysAgo = new Date(today);
-        tenDaysAgo.setDate(today.getDate() - 10);
+	try {
+		const today = new Date();
+		const tenDaysAgo = new Date(today);
+		tenDaysAgo.setDate(today.getDate() - 10);
 
-        const dailyData = await Restaurant.aggregate([
-            {
-                $match: {
-                    createdAt: {
-                        $gte: tenDaysAgo,
-                        $lt: today
-                    }
-                }
-            },
-            {
-                $group: {
-                    _id: {
-                        day: { $dayOfMonth: '$createdAt' },
-                        month: { $month: '$createdAt' },
-                        year: { $year: '$createdAt' }
-                    },
-                    total: { $sum: 1 }
-                }
-            },
-            {
-                $project: {
-                    _id: 0,
-                    date: {
-                        $concat: [
-                            { $toString: '$_id.month' },
-                            '/',
-                            { $toString: '$_id.day' },
-                            '/',
-                            { $toString: '$_id.year' }
-                        ]
-                    },
-                    total: 1
-                }
-            },
-            { $sort: { 'date': 1 } }
-        ]);
+		const dailyData = await Restaurant.aggregate([
+			{
+				$match: {
+					createdAt: {
+						$gte: tenDaysAgo,
+						$lte: today, // Change $lt to $lte to include the current date
+					},
+				},
+			},
+			{
+				$group: {
+					_id: {
+						day: { $dayOfMonth: '$createdAt' },
+						month: { $month: '$createdAt' },
+						year: { $year: '$createdAt' },
+					},
+					total: { $sum: 1 },
+				},
+			},
+			{
+				$project: {
+					_id: 0,
+					date: {
+						$concat: [{ $toString: '$_id.month' }, '/', { $toString: '$_id.day' }, '/', { $toString: '$_id.year' }],
+					},
+					total: 1,
+				},
+			},
+			{ $sort: { date: 1 } },
+		]);
 
-        res.status(200).json(dailyData);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error fetching daily restaurant data' });
-    }
+		res.status(200).json(dailyData);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: 'Error fetching daily restaurant data' });
+	}
 });
 
 // Endpoint for one restaurant by Slug
