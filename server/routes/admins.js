7648b3459bcd61a01ada3892b7df8701d0c99406
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2023-04-03 01:15:45 -0700

    Possible fix for admin routes

--- server/routes/admins.js
+++ server/routes/admins.js
@@ -2,11 +2,12 @@ const express = require('express');
 const Restaurant = require('../models/resto');
 const router = express.Router();
 const dummyAuth = require('../middlewares/dummyAuth');
+const ensureAdmin = require('../middlewares/ensureAdmin');
 const { addRestaurant, addDish, updateRestaurant } = require('../controllers/restaurants');
 
 // Add a new restaurant
-router.post('/', dummyAuth, addRestaurant);
+router.post('/', dummyAuth, ensureAdmin, addRestaurant);
 // Update a restaurant by ID
-router.put('/:restaurantId', dummyAuth, updateRestaurant);
+router.put('/:restaurantId', dummyAuth, ensureAdmin, updateRestaurant);
 // Add a new dish to a restaurant
-router.post('/:restaurantId/dishes', dummyAuth, addDish);
+router.post('/:restaurantId/dishes', dummyAuth, ensureAdmin, addDish);
