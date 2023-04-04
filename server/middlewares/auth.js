7dd83f48bda199c6bceba84c47f0f0cb38847c19
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2023-04-04 00:48:10 -0700

    Disable auth for now

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,10 +2,9 @@ const { UnauthorizedError } = require('../errors');
 
 // Ensures that the user is authenticated
 function ensureAuth(req, res, next) {
-	if (req.isAuthenticated()) {
-		return next();
-	}
-	return next(new UnauthorizedError('Please log in to access this resource'));
+	// TODO: Replace this with proper authentication using JWT or similar.
+	console.warn('Warning: Authentication is currently disabled. Implement proper authentication before deploying to production.');
+	return next();
 }
 
 module.exports = ensureAuth;
